776a77e089d7ef8c5fe3fb45cbf38e978b4ff00a
Robert Jomar Malate <robjmal@gmail.com>
2024-05-24 15:40:42 +0200

    Added extra libraries to help with designing controller

--- task1a/CableActuator/FingerControl.py
+++ task1a/CableActuator/FingerControl.py
@@ -1,5 +1,13 @@
 # Required import for SOFA within python
 import Sofa
+import Sofa.Core
+import Sofa.Simulation
+import Sofa.Gui
+
+# -*- coding: utf-8 -*-
+import os
+
+path = os.path.dirname(os.path.abspath(__file__)) + '/mesh/'
 
 
 def main():
@@ -12,15 +20,117 @@ def main():
         # Once defined, initialization of the scene graph
         Sofa.Simulation.init(root)
 
-        # Run as many simulation steps (here 10 steps are computed)
-        for iteration in range(10):
-                Sofa.Simulation.animate(root, root.dt.value)
+        # Launch the GUI (qt or qglviewer)
+        Sofa.Gui.GUIManager.Init("myscene", "qglviewer")
+        Sofa.Gui.GUIManager.createGUI(root, __file__)
+        Sofa.Gui.GUIManager.SetDimension(1080, 800)
+
+        # Initialization of the scene will be done here
+        Sofa.Gui.GUIManager.MainLoop(root)
+        Sofa.Gui.GUIManager.closeGUI()
+        print("Closed GUI")
 
 
-# Same createScene function as in the previous case
+# # Same createScene function as in the previous case
+# def createScene(rootNode):
+#         #Doesn't do anything yet
+#         return rootNode
+
 def createScene(rootNode):
-        #Doesn't do anything yet
-        return rootNode
+    rootNode.addObject('RequiredPlugin',
+                       pluginName='SoftRobots SoftRobots.Inverse')
+    rootNode.addObject('VisualStyle',
+                       displayFlags='showVisualModels hideBehaviorModels showCollisionModels '
+                                    'hideBoundingCollisionModels hideForceFields showInteractionForceFields '
+                                    'hideWireframe')
+
+    rootNode.addObject('FreeMotionAnimationLoop')
+    rootNode.addObject('QPInverseProblemSolver', printLog=False)
+
+    rootNode.gravity = [0, -9180, 0]
+
+    ##########################################
+    # FEM Model                              #
+    ##########################################
+    finger = rootNode.addChild('finger')
+    finger.addObject('EulerImplicitSolver', firstOrder=True, rayleighMass=0.1, rayleighStiffness=0.1)
+    finger.addObject('SparseLDLSolver', template="CompressedRowSparseMatrixMat3x3d")
+    finger.addObject('MeshVTKLoader', name='loader', filename=path + 'finger.vtk')
+    finger.addObject('MeshTopology', src='@loader', name='container')
+    finger.addObject('MechanicalObject')
+    finger.addObject('UniformMass', totalMass=0.075)
+    finger.addObject('TetrahedronFEMForceField', poissonRatio=0.3, youngModulus=600)
+    finger.addObject('BoxROI', name='ROI1', box=[-15, 0, 0, 5, 10, 15], drawBoxes=True)
+    finger.addObject('RestShapeSpringsForceField', points='@ROI1.indices', stiffness=1e12)
+    finger.addObject('LinearSolverConstraintCorrection')
+
+    ##########################################
+    # Visualization                          #
+    ##########################################
+    fingerVisu = finger.addChild('visu')
+    fingerVisu.addObject('MeshSTLLoader', filename=path + "finger.stl", name="loader")
+    fingerVisu.addObject('OglModel', src="@loader", template='Vec3', color=[0.0, 0.7, 0.7, 1])
+    fingerVisu.addObject('BarycentricMapping')
+
+    ##########################################
+    # Cable                                  #
+    ##########################################
+    cable = finger.addChild('cable')
+    cable.addObject('MechanicalObject',
+                    position=[
+                        [-17.5, 12.5, 2.5],
+                        [-32.5, 12.5, 2.5],
+                        [-47.5, 12.5, 2.5],
+                        [-62.5, 12.5, 2.5],
+                        [-77.5, 12.5, 2.5],
+
+                        [-83.5, 12.5, 4.5],
+                        [-85.5, 12.5, 6.5],
+                        [-85.5, 12.5, 8.5],
+                        [-83.5, 12.5, 10.5],
+
+                        [-77.5, 12.5, 12.5],
+                        [-62.5, 12.5, 12.5],
+                        [-47.5, 12.5, 12.5],
+                        [-32.5, 12.5, 12.5],
+                        [-17.5, 12.5, 12.5]
+                    ])
+
+    # Set a maximum displacement for your cable
+    cable.addObject('CableActuator', name="aCable",
+                    indices=list(range(0, 14)),
+                    pullPoint=[0.0, 12.5, 2.5],
+                    maxPositiveDisp=40,
+                    maxDispVariation=0.5,
+                    minForce=0)
+
+    cable.addObject('BarycentricMapping')
+
+    ##########################################
+    # Effector goal for interactive control  #
+    ##########################################
+    goal = rootNode.addChild('goal')
+    goal.addObject('EulerImplicitSolver', firstOrder=True)
+    goal.addObject('CGLinearSolver', iterations=100, tolerance=1e-5, threshold=1e-5)
+    goal.addObject('MechanicalObject', name='goalMO',
+                   position=[-120, 7, 7])
+    goal.addObject('SphereCollisionModel', radius=5)
+    goal.addObject('UncoupledConstraintCorrection')
+
+    ##########################################
+    # Effector                               #
+    ##########################################
+    effector = finger.addChild('fingertip')
+    effector.addObject('MechanicalObject', position=([-103, 7, 7]))
+    effector.addObject('PositionEffector', template='Vec3',
+                       indices=0,
+                       effectorGoal="@../../goal/goalMO.position")
+    effector.addObject('BarycentricMapping', mapForces=False, mapMasses=False)
+
+    print("****** Trying to print out the goal states ******")
+    print(goal.goalMO.position.value)
+    print("******************")
+    return rootNode
 
 
 # Function used only if this script is called from a python environment
