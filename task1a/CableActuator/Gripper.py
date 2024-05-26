d2bd6cae8ed2c18b2852a576a54c0312402c558e
Robert Jomar Malate <robjmal@gmail.com>
2024-05-26 16:26:59 +0200

    Able to get the other cable for the other gripper to display, but they are not lining up

--- task1a/CableActuator/Gripper.py
+++ task1a/CableActuator/Gripper.py
@@ -47,6 +47,56 @@ def createScene(rootNode):
 
     rootNode.gravity = [0, -9180, 0]
 
+    # Setup
+    default_cable_position = [
+                         [-17.5, 12.5, 2.5], 
+                         [-32.5, 12.5, 2.5], 
+                         [-47.5, 12.5, 2.5], 
+                         [-62.5, 12.5, 2.5], 
+                         [-77.5, 12.5, 2.5], 
+
+                         [-83.5, 12.5, 4.5], 
+                         [-85.5, 12.5, 6.5], 
+                         [-85.5, 12.5, 8.5], 
+                         [-83.5, 12.5, 10.5], 
+
+                         [-77.5, 12.5, 12.5], 
+                         [-62.5, 12.5, 12.5], 
+                         [-47.5, 12.5, 12.5], 
+                         [-32.5, 12.5, 12.5], 
+                         [-17.5, 12.5, 12.5]
+                        ]
+
+    def calculate_rotated_matrix(points_matrix, theta=0.0, axis='y'): 
+        '''
+        theta: Angle to rotate [radians]
+        '''
+
+        rotation_matrix = np.eye(3)
+
+        if axis=='y':
+            rotation_matrix = [
+                [np.cos(theta), 0, np.sin(theta)],
+                [0, 1, 0],
+                [-np.sin(theta), 0, np.cos(theta)],
+            ]
+        rotation_matrix = np.array(rotation_matrix)
+
+        rotated_matrix = [np.dot(rotation_matrix, point).tolist() for point in points_matrix]
+        
+        return rotated_matrix
+    
+
+    default_cable_pull_point = [[0.0, 12.5, 2.5]]
+
+    cable2_rotation = np.pi/4
+    cable2_position = calculate_rotated_matrix(default_cable_position, -1*cable2_rotation)
+    cable2_pull_point = calculate_rotated_matrix(default_cable_pull_point, cable2_rotation)
+
+    # cable3_position = calculate_rotated_matrix(default_cable_position, -np.pi/4)
+
+    # breakpoint()
+
     ##########################################
     # First Finger                           #
     ##########################################
@@ -62,43 +112,64 @@ def createScene(rootNode):
     finger1.addObject('RestShapeSpringsForceField', points='@ROI1.indices', stiffness=1e12)
     finger1.addObject('LinearSolverConstraintCorrection')
 
-    finger1Visu = finger1.addChild('visu')
+    finger1Visu = finger1.addChild('visu1')
     finger1Visu.addObject('MeshSTLLoader', filename=path + "finger.stl", name="loader")
     finger1Visu.addObject('OglModel', src="@loader", template='Vec3', color=[0.0, 0.7, 0.7, 1])
     finger1Visu.addObject('BarycentricMapping')
 
     cable1 = finger1.addChild('cable')
-    cable1.addObject('MechanicalObject', 
-                     position=[
-                         [-17.5, 12.5, 2.5], 
-                         [-32.5, 12.5, 2.5], 
-                         [-47.5, 12.5, 2.5], 
-                         [-62.5, 12.5, 2.5], 
-                         [-77.5, 12.5, 2.5], 
-                         [-83.5, 12.5, 4.5], 
-                         [-85.5, 12.5, 6.5], 
-                         [-85.5, 12.5, 8.5], 
-                         [-83.5, 12.5, 10.5], 
-                         [-77.5, 12.5, 12.5], 
-                         [-62.5, 12.5, 12.5], 
-                         [-47.5, 12.5, 12.5], 
-                         [-32.5, 12.5, 12.5], 
-                         [-17.5, 12.5, 12.5]
-                         ])
+    cable1.addObject('MechanicalObject', position=default_cable_position)
     cable1.addObject('CableActuator', name="aCable", indices=list(range(0, 14)), pullPoint=[0.0, 12.5, 2.5], maxPositiveDisp=40, maxDispVariation=0.5, minForce=0)
     cable1.addObject('BarycentricMapping')
 
-    goal1 = rootNode.addChild('goal1')
-    goal1.addObject('EulerImplicitSolver', firstOrder=True)
-    goal1.addObject('CGLinearSolver', iterations=100, tolerance=1e-5, threshold=1e-5)
-    goal1.addObject('MechanicalObject', name='goalMO', position=[-120, 7, 7])
-    goal1.addObject('SphereCollisionModel', radius=5)
-    goal1.addObject('UncoupledConstraintCorrection')
+    # goal1 = rootNode.addChild('goal1')
+    # goal1.addObject('EulerImplicitSolver', firstOrder=True)
+    # goal1.addObject('CGLinearSolver', iterations=100, tolerance=1e-5, threshold=1e-5)
+    # goal1.addObject('MechanicalObject', name='goalMO', position=[-120, 7, 7])
+    # goal1.addObject('SphereCollisionModel', radius=5)
+    # goal1.addObject('UncoupledConstraintCorrection')
 
-    effector1 = finger1.addChild('fingertip')
-    effector1.addObject('MechanicalObject', position=([-103, 7, 7]))
-    effector1.addObject('PositionEffector', template='Vec3', indices=0, effectorGoal="@../../goal1/goalMO.position")
-    effector1.addObject('BarycentricMapping', mapForces=False, mapMasses=False)
+    # effector1 = finger1.addChild('fingertip')
+    # effector1.addObject('MechanicalObject', position=([-103, 7, 7]))
+    # effector1.addObject('PositionEffector', template='Vec3', indices=0, effectorGoal="@../../goal1/goalMO.position")
+    # effector1.addObject('BarycentricMapping', mapForces=False, mapMasses=False)
+
+    ##########################################
+    # Second Finger                          #
+    ##########################################
+    finger2 = rootNode.addChild('finger2')
+    finger2.addObject('EulerImplicitSolver', firstOrder=True, rayleighMass=0.1, rayleighStiffness=0.1)
+    finger2.addObject('SparseLDLSolver', template="CompressedRowSparseMatrixMat3x3d")
+    finger2.addObject('MeshVTKLoader', name='loader', filename=path + 'finger.vtk')
+    finger2.addObject('MeshTopology', src='@loader', name='container')
+    finger2.addObject('MechanicalObject', rotation2=[0.0, 0.0, 0.0])  # Adjust position and rotation
+    finger2.addObject('UniformMass', totalMass=0.075)
+    finger2.addObject('TetrahedronFEMForceField', poissonRatio=0.3, youngModulus=600)
+    finger2.addObject('BoxROI', name='ROI2', box=[-15, 0, 0, 5, 10, 15], drawBoxes=True)
+    finger2.addObject('RestShapeSpringsForceField', points='@ROI2.indices', stiffness=1e12)
+    finger2.addObject('LinearSolverConstraintCorrection')
+
+    finger2Visu = finger2.addChild('visu2')
+    finger2Visu.addObject('MeshSTLLoader', filename=path + "finger.stl", name="loader")
+    finger2Visu.addObject('OglModel', src="@loader", template='Vec3', color=[0.0, 0.7, 0.0, 1])
+    finger2Visu.addObject('BarycentricMapping')
+
+    cable2 = finger2.addChild('cable2')
+    cable2.addObject('MechanicalObject', position=cable2_position)
+    cable2.addObject('CableActuator', name="aCable2", indices=list(range(0, 14)), pullPoint=[0.0, 12.5, 2.5], maxPositiveDisp=40, maxDispVariation=0.5, minForce=0)
+    cable2.addObject('BarycentricMapping')
+
+    # goal2 = rootNode.addChild('goal2')
+    # goal2.addObject('EulerImplicitSolver', firstOrder=True)
+    # goal2.addObject('CGLinearSolver', iterations=100, tolerance=1e-5, threshold=1e-5)
+    # goal2.addObject('MechanicalObject', name='goalMO', position=[103, 7, 7])
+    # goal2.addObject('SphereCollisionModel', radius=5)
+    # goal2.addObject('UncoupledConstraintCorrection')
+
+    # effector2 = finger2.addChild('fingertip')
+    # effector2.addObject('MechanicalObject', position=([103, 7, 7]))
+    # effector2.addObject('PositionEffector', template='Vec3', indices=0, effectorGoal="@../../goal2/goalMO.position")
+    # effector2.addObject('BarycentricMapping', mapForces=False, mapMasses=False)
 
     return rootNode
 
